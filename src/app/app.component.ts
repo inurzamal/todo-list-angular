eb7e5b3a0ff3f60b33616c10850797fd3e39f2f2
Islam <nurzamal.islam@capgemini.com>
2022-12-26 19:57:01 +0530

    todo-list 0.0.1-basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  
+  // tasks: string[] = [];
+  // task_s = []
+
+  tasks: Task[] = [
+    new Task('Practice Angular Project'),
+    new Task('Learn NodeJs from Training Videos & YT'),
+    new Task('Java-React Programming & Kafka Implementation'),
+    new Task('Cover all other Java topics from notes'),
+    new Task('Pesto Training')
+  ]
+
+  add(newTask:string){ //not persisted yet
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(existingTask: Task){ //you can use id here
+    var userConfirmed = confirm(`Are you sure, you want to remove the following task? \n ${existingTask.title}`)
+
+    if(userConfirmed){
+      this.tasks = this.tasks.filter(task=>task != existingTask);
+    }
+  }
+
+}
+
+class Task{
+
+  constructor(public title:string){
+
+  }
+
+  public isDone = false;
+
+  toggleIsDone(){
+
+    this.isDone = !this.isDone;
+
+  }
+
 }
